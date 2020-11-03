392eeb430c8626cf1e01163c4d067f52bee0c58e
Vadim Vereketa <kam1cadzee@gmail.com>
2020-11-03 13:10:03 +0200

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,4 @@
 $(document).ready(function () {
-
-    var pano2 = new pano2vrPlayer("container2");
-    var pano1 = new pano2vrPlayer("container1");
-    var pano3 = new pano2vrPlayer("container3");
-    // load the configuration
-
-    window.addEventListener("load", function () {
-        pano2.readConfigUrlAsync("pano2.xml");
-        pano1.readConfigUrlAsync("pano1.xml");
-        pano3.readConfigUrlAsync("pano3.xml");
-    });
-
     function initAboutUsBlock() {
         if($(window).width() >= 992) {
             $('.about-us_link').on('click', function () {
---
+++ js/view.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  //var pano2 = new pano2vrPlayer("container2");
+  var pano1 = new pano2vrPlayer("container1");
+  //var pano3 = new pano2vrPlayer("container3");
+  // load the configuration
+
+  window.addEventListener("load", function () {
+    //pano2.readConfigUrlAsync("pano2.xml");
+    pano1.readConfigUrlAsync("pano1.xml");
+    //pano3.readConfigUrlAsync("pano3.xml");
+  });
+});
