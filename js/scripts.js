06adad05d5d6b4aa1ad1fedd37f300a1606ad4f4
Vadim Vereketa <kam1cadzee@gmail.com>
2020-11-03 11:58:00 +0200

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,15 @@ $(document).ready(function () {
     initAboutUsBlock();
     $(window).resize(initAboutUsBlock);
 
+    $(window).scroll(function () {
+        var windScroll = $(window).scrollTop();
+        if (windScroll >= 1) {
+            $('.header').addClass("scroll");
+        } else {
+            $('.header').removeClass("scroll");
+        }
+    }).scroll();
+
 
     //scroll to top button
     var btn = $('#button');
